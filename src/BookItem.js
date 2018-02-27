fd6b15e4d5751cbdba07ee56bac5b98d1d6405fc
James Vuong <jvuong@mckinley.com>
2018-02-26 21:49:37 -0500

    Make search work correctly when a book does not have a thumbnail

--- src/BookItem.js
+++ src/BookItem.js
@@ -5,11 +5,16 @@ class BookItem extends Component {
     render() {
         const { book, handleBookShelfChange } = this.props
         let authors = (typeof book.authors !== 'undefined') ? book.authors.join(", ") : "";
+        var bookCoverStyle = { width: 128, height: 193 }
+        if(typeof(book.imageLinks) !== 'undefined') {
+            bookCoverStyle.backgroundImage = `url("${book.imageLinks.thumbnail}")`
+        } 
+        
         return (
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                        <div className="book-cover" style={bookCoverStyle}></div>
                         <BookShelfChanger shelf={book.shelf} handleBookShelfChange={newShelf => handleBookShelfChange(newShelf)} />
                     </div>
                     <div className="book-title">{ book.title }</div>
